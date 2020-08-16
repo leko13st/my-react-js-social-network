deda154ae2ccf9b6ce9790f566faf53d141758f0
leko13st <lekomstas13@mail.ru>
2020-08-16 15:47:20 +0400

    added iteraction with API server on users' page

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { createRef } from 'react';
+import * as axios from 'axios';
+import photoUser from './../../assets/images/FyIwsk1S_400x400.jpg'
 
 let Users = (props) => {
+
+    if (props.users.length === 0){
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then((response) => {
+            props.setUsers(response.data.items)
+        })
+    }
 
     let buttonRef = React.createRef();
 
@@ -17,7 +26,7 @@ let Users = (props) => {
                 <div key={user.id}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt="papich-ava" width='10%' />
+                            <img src={(user.photos.small == null) ? photoUser : user.photos.small} alt="papich-ava" width='10%' />
                         </div>
                         <div>
                             <button onClick={() => follow_toogle(user.id)}>{user.followed ? 'Unfollow' : 'Follow'}</button>
@@ -25,12 +34,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>
+                            <div>{'user.location.country'}</div>
+                            <div>{'user.location.city'}</div>
                         </span>
                     </span>
                 </div>
--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,9 +3,9 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
     users: [
-        {id: 1, photoUrl: 'https://sun9-14.userapi.com/impf/c847123/v847123629/16d660/w9Hu6NWRFQ8.jpg?size=200x0&quality=90&sign=5e976c09b94b30bcfdc828b6e0396c95&ava=1', followed: true, fullName: 'Jhon', status: 'I\'m busy', location: {country: 'Russia', city: 'Perm'}},
-        {id: 2, photoUrl: 'https://game2day.ru//images/made/7b4f3a1209326b54/papich8_360_208_s_c1.jpg', followed: false, fullName: 'Vitaly Tsal', status: 'I\'m streaming', location: {country: 'Ukraine', city: 'Vinnitsa'}},
-        {id: 3, photoUrl: 'https://pbs.twimg.com/profile_images/1060286056342474752/FyIwsk1S_400x400.jpg', followed: true, fullName: 'Microchel', status: 'Arthas\' fan', location: {country: 'Russia', city: 'Moscow'}}
+        // {id: 1, photoUrl: 'https://sun9-14.userapi.com/impf/c847123/v847123629/16d660/w9Hu6NWRFQ8.jpg?size=200x0&quality=90&sign=5e976c09b94b30bcfdc828b6e0396c95&ava=1', followed: true, fullName: 'Jhon', status: 'I\'m busy', location: {country: 'Russia', city: 'Perm'}},
+        // {id: 2, photoUrl: 'https://game2day.ru//images/made/7b4f3a1209326b54/papich8_360_208_s_c1.jpg', followed: false, fullName: 'Vitaly Tsal', status: 'I\'m streaming', location: {country: 'Ukraine', city: 'Vinnitsa'}},
+        // {id: 3, photoUrl: 'https://pbs.twimg.com/profile_images/1060286056342474752/FyIwsk1S_400x400.jpg', followed: true, fullName: 'Microchel', status: 'Arthas\' fan', location: {country: 'Russia', city: 'Moscow'}}
     ]
 }
 
