8d1cb15a199917b017b1738a5254d75b153a5c30
leko13st <lekomstas13@mail.ru>
2020-08-28 16:59:55 +0500

    used 'reselect' module

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -5,6 +5,7 @@ import Users from "./Users";
 import Preloader from '../common/Preloader/Preloader';
 import withAuthRedirect from '../../hoc/withAuthRedirect';
 import { compose } from 'redux';
+import { getUsersSuper, getPageSize, getCurrentPage, getTotalUsersCount, getIsFetching, getFollowingProgress } from '../../Redux/users-selectors';
 
 class UsersContainer extends React.Component{
     
@@ -38,17 +39,27 @@ class UsersContainer extends React.Component{
     }
 }
 
+// const mapStateToProps = (state) => {
+//     return {
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingProgress: state.usersPage.followingProgress
+//     }
+// }
+
 const mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingProgress: state.usersPage.followingProgress
+        users: getUsersSuper(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingProgress: getFollowingProgress(state)
     }
 }
-
 // const disp = (dispatch) => {
 //     return{
 //         func: (body) => dispatch(funcAC(body))
---
+++ src/Redux/users-selectors.js
@@ -0,0 +1,29 @@
+import { createSelector } from 'reselect';
+
+export const getUsers = (state) => {
+    return state.usersPage.users;
+}
+
+export const getUsersSuper = createSelector(getUsers, (users) => {
+    return users;
+})
+
+export const getPageSize = (state) => {
+    return state.usersPage.getPageSize;
+}
+
+export const getTotalUsersCount = (state) => {
+    return state.usersPage.totalUsersCount;
+}
+
+export const getCurrentPage = (state) => {
+    return state.usersPage.currentPage;
+}
+
+export const getIsFetching = (state) => {
+    return state.usersPage.isFetching;
+}
+
+export const getFollowingProgress = (state) => {
+    return state.usersPage.followingProgress;
+}
